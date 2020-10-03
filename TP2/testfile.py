c1b7db2b9945d0d498bf5ec27105135a7ea50ffd
cedric <cedric.sadeu@polymtl.ca>
2020-10-02 20:19:37 -0400

    2.a 2.b 2.c 2.d

--- TP2/testfile.py
+++ TP2/testfile.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 from collections import Counter
 import re
+import pandas as pd
 #Ecrire une fonction qui retourne les +/- 5 voisins du mot en parametres
 
 x = ["vous", "ecrire", "une", "fonction", "qui", "retourne", "les", "voisins", "du", "mot"]
@@ -69,9 +70,9 @@ def neighbours_grapper(list_words, word, N, is_distance):
     return result
 
 
-def neighbours_corpus(corpus, N, frame_panda, is_distance):
+def neighbours_corpus(corpus, N, unigramme_list, is_distance):
     result = []
-    for word in frame_panda:
+    for word in unigramme_list:
         for sentence in corpus:
             re.split(r'\s', sentence)
             result.extend(neighbours_grapper(sentence, word, N, is_distance))
@@ -89,7 +90,6 @@ def dict_tuple_creator_d(tuple_list):
     res = list(zip(*tuple_list))
     tup = list(res[0])
     dict_tup = Counter(tup)
-    print(dict_tup)
     for key in dict_tup.keys():
         temp = [item[1] for item in tuple_list if item[0] == key]
         total = sum(temp)
@@ -98,13 +98,14 @@ def dict_tuple_creator_d(tuple_list):
 
 print(dict_tuple_creator_d(test))
 
-def matrix_creator(dict_tuple, unigramme_list, N):
+def matrix_creator(dict_tuple, unigramme_list):
     # avoir un dict avec key (mot1 , mot2) et frequence comme value
     # creer une matrice 5000 * 5000 et la remplir de zero
     # parcourir le dictionnaire:
     #   pour chaque key (mot1 , mot2): find la position du mot1 et du mot2 dans la matrice
     #                           pour avoir ces coordonnes (pos1, pos2) dans la matrice
     # mettre la value correspondante a cette key a (pos1, pos2) dans la matrice
+    N = len(unigramme_list)
     list_zero = [0] * N
     mat_result = [list_zero] * N
     for key in dict_tuple.keys():
@@ -115,7 +116,19 @@ def matrix_creator(dict_tuple, unigramme_list, N):
         except ValueError:
             continue
         mat_result[x][y] = dict_tuple[key]
+    
     return mat_result
+
+def mat_list_to_df(mat_result, unigramme_list):
+    arr = np.array(mat_result)
+    t_arr = arr.T
+    arr_list = t_arr.tolist()
+    dict_result = {}
+    #dict_result["unigrammesMots"] = unigramme_list
+    for i, word in enumerate(unigramme_list):
+        dict_result[word] = arr_list[i]
+    df = pd.DataFrame(data = dict_result, index = unigramme_list)
+    return df
 
 def probability_matrix(mat_Result):
     total = 0
@@ -125,7 +138,12 @@ def probability_matrix(mat_Result):
     arr_prob = np.divide(arr, total)
     return arr_prob.tolist()
 
-def ppmi_pmi(mat_prob, is_ppmi):
+def ppmi_pmi(df_Result, is_ppmi):
+    #col = df_Result.columns[0]
+    #temp_dict = df_Result.set_index(col).T.to_dict('list')
+    temp = df_Result.to_numpy()
+    mat_Result = list(temp.tolist())
+    mat_prob = probability_matrix(mat_Result)
     arr = np.array(mat_prob)
     sum_arr_column = arr.sum(axis=0)
     sum_arr_line = arr.sum(axis=1)
@@ -137,11 +155,40 @@ def ppmi_pmi(mat_prob, is_ppmi):
                 mat_prob[i][j] = max(math.log(item / (sum_arr_column[j]*sum_arr_line[i]), 2), 0)
             else:
                 mat_prob[i][j] = math.log(item / (sum_arr_column[j]*sum_arr_line[i]), 2)
-    return mat_prob
+    
+    return mat_list_to_df(mat_prob, list(df_Result.columns))
 
 
 mat_prob = [[0.0, 0.0, 0.05, 0.0, 0.05], [0, 0, 0.05, 0, 0.05], [0.11, 0.05, 0, 0.05, 0.0], [0.05, 0.32, 0, 0.21, 0]]
 
 print(ppmi_pmi(mat_prob, False))
 
-    
+#A FAIRE:
+
+# modifier  Squelette => 20 min
+
+# 2.A
+tuple_list = neighbours_corpus(corpus, 5, unigramme_list, False)
+dict_tuple = dict_tuple_creator(tuple_list)
+mat_result = matrix_creator(dict_tuple, unigramme_list)
+df = mat_list_to_df(mat_result, unigramme_list)
+df.to_csv('tp2_mat5.csv')
+
+# 2.B
+tuple_list_d = neighbours_corpus(corpus, 5, unigramme_list, True)
+dict_tuple_d = dict_tuple_creator_d(tuple_list_d)
+mat_result_d = matrix_creator(dict_tuple_d, unigramme_list)
+df_d = mat_list_to_df(mat_result_d, unigramme_list)
+df_d.to_csv('p2_mat5_scaled.csv')
+
+# 2.D
+result_ppmi_df = ppmi_pmi(df, True)
+result_ppmi_df.to_csv('tp2_mat5_ppmi.csv')
+result_pmi_df = ppmi_pmi(df, False)
+result_pmi_df.to_csv('tp2_mat5_pmi.csv')
+
+
+result_ppmi_sc_df = ppmi_pmi(df_d, True)
+result_ppmi_sc_df.to_csv('tp2_mat5_scaled_ppmi.csv')
+result_pmi_sc_df = ppmi_pmi(df_d, False)
+result_pmi_sc_df.to_csv('tp2_mat5_scaled_pmi.csv')
